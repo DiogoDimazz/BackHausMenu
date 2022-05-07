79b5ae83f6a4e2df7cf0442877fdd5f5c366de44
Dimazz <diogo.compositor@gmail.com>
2022-05-06 23:33:41 -0300

    porta dinâmica configurada

--- src/connection.js
+++ src/connection.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        user: 'azradcexqnrsws',
         host: 'ec2-18-210-64-223.compute-1.amazonaws.com',
+        user: 'azradcexqnrsws',
         password: 'bcf93ef761e5d782878199bfb7f1fc87e67bd3b6979fa89bf547a942f417a7e7',
-        database: 'd98r7s6m0tb6al',
         port: 5432,
+        database: 'd98r7s6m0tb6al',
         ssl: {
             rejectUnauthorized: false
         }
