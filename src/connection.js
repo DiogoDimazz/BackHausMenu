07dabd26a18a8593dabb429c3d83544a70d12609
Dimazz <diogo.compositor@gmail.com>
2022-05-07 22:47:06 -0300

    teste local

--- src/connection.js
+++ src/connection.js
@@ -5,6 +5,11 @@ const knex = require('knex')({
         user: 'postgres',
         password: 'postgres',
         port: 5432
+    }
+})
+
+module.exports = knex;
+
         // host: 'ec2-18-210-64-223.compute-1.amazonaws.com',
         // user: 'azradcexqnrsws',
         // password: 'bcf93ef761e5d782878199bfb7f1fc87e67bd3b6979fa89bf547a942f417a7e7',
@@ -13,7 +18,3 @@ const knex = require('knex')({
         // ssl: {
         //     rejectUnauthorized: false
         // }
-    }
-})
-
-module.exports = knex;
