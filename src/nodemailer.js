9c711050b474c5139680d5250c49e757cb49e602
Dimazz <diogo.compositor@gmail.com>
2022-05-09 16:27:33 -0300

    mailtrap

--- src/nodemailer.js
+++ src/nodemailer.js
@@ -1,12 +1,11 @@
 const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.mailgun.org',
-    port: 587,
-    secure: false,
+    host: 'smtp.mailtrap.io',
+    port: 2525,
     auth: {
-        user: 'postmaster@hausmenu.com.br',
-        pass: 'd6e7f8a253b08b6ac398cb4b4f6ac611-100b5c8d-88f09b23'
+        user: 'e4cc708276111a',
+        pass: '8b2cc3affb8252'
     }
 })
 
