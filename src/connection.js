f833460e671988d12eb2f49e3be0c2a6e5246155
Dimazz <diogo.compositor@gmail.com>
2022-05-09 11:30:02 -0300

    configuração do banco de dados digital ocean

--- src/connection.js
+++ src/connection.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'ec2-18-210-64-223.compute-1.amazonaws.com',
-        user: 'azradcexqnrsws',
-        password: 'bcf93ef761e5d782878199bfb7f1fc87e67bd3b6979fa89bf547a942f417a7e7',
-        port: 5432,
-        database: 'd98r7s6m0tb6al',
+        host: 'hausmenu-db-do-user-11521879-0.b.db.ondigitalocean.com',
+        user: 'doadmin',
+        password: 'AVNS_vHWJUNGTUD4AC-O',
+        port: 25060,
+        database: 'defaultdb',
         ssl: {
             rejectUnauthorized: false
         }
