f1d4cbcb936200e0010cebbc495f15765213834b
Dimazz <diogo.compositor@gmail.com>
2022-08-22 15:11:45 -0300

    teste_3

--- src/connection.js
+++ src/connection.js
@@ -1,14 +1,26 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: DB_HOST,
-        user: DB_USER,
-        password: DB_PASSWORD,
-        database: DB_DATABASE,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         ssl: {
             rejectUnauthorized: false
         }
     }
 })
+
+// const knex = require('knex')({
+//     client: 'pg',
+//     connection: {
+//         host: 'localhost',
+//         user: 'postgres',
+//         password: 'postgres',
+//         port: 5432,
+//         database: 'hausmenu'
+//     }
+// })
+
 
 module.exports = knex;
