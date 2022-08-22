fd241b3a4bd246b867794ff6a8b6895bc9718cfb
Dimazz <diogo.compositor@gmail.com>
2022-08-22 15:41:38 -0300

    login_ok

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -6,7 +6,6 @@ const jwt = require('jsonwebtoken')
 const login = async (req, res) => {
     const { email, password } = req.body;
 
-    console.log('teste 9');
 
     try {
         const bodyFormatted = {
@@ -14,23 +13,19 @@ const login = async (req, res) => {
             password: password.trim()
         }
 
-        console.log('teste 17');
 
         await loginSchema.validate(bodyFormatted)
 
-        console.log('teste 21');
 
         const userFound = await knex('usuarios').where({ email }).first();
         if (!userFound) { return res.status(404).json("Invalid email or password!") }
 
         const passwordCompare = await bcrypt.compare(password, userFound.senha)
-        if (!passwordCompare) { return res.status(400).json('Invalid email or password') }
+        if (!passwordCompare) { return res.status(400).json('Invalid email or password!') }
 
-        console.log('teste 29');
 
         const token = jwt.sign({ id: userFound.id }, process.env.HASHCODE, { expiresIn: '12h' })
 
-        console.log('teste 33');
 
         return res.status(200).json({
             id: userFound.id,
@@ -39,7 +34,6 @@ const login = async (req, res) => {
         })
 
     } catch (error) {
-        console.log('teste 42');
         return res.status(400).json(error.message)
     }
 }
